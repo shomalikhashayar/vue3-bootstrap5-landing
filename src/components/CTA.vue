9a97e78beedb3a8613c0c316a9f3cd189e7fab98
Khashayar Shomali <khashi792017@gmail.com>
2023-01-10 18:52:10 +0330

    Add link to buttons

--- src/components/CTA.vue
+++ src/components/CTA.vue
@@ -1,11 +1,27 @@
 <template>
-<div class="container">
+  <div class="container">
     <div class="row">
-        <div class="col">
-            <div class="bg-secondary">tryhthtt</div>
+      <div class="col">
+        <div class="bg-white my-5 py-4">
+          Lorem ipsum dolor sit amet. Et amet saepe ab quae sunt et vero ipsa in
+          labore consequatur! Quo ratione quasi hic maxime laudantium et
+          reiciendis repudiandae rem exercitationem architecto. Qui distinctio
+          incidunt ut tempore quos eum fugiat ipsam aut molestiae debitis eum
+          quia magni aut aspernatur iste ex tenetur consequatur. Non beatae
+          doloribus est enim voluptas et deserunt dolor et perferendis
+          exercitationem. Qui voluptatem ullam non quos voluptatem est
+          doloremque soluta. Quo recusandae delectus aut sapiente voluptas cum
+          dolor excepturi a repellendus itaque. Eos perferendis quis et
+          aspernatur laudantium et commodi sunt ut error sapiente aut illum
+          fuga. Sit voluptatem perferendis et tempore sequi sed nobis galisum
+          eum iusto doloremque. Aut dolores velit ex fugiat aliquam et unde
+          distinctio aut nihil internos ab dignissimos exercitationem qui ullam
+          repudiandae. Et minus reprehenderit aut dolor culpa ut voluptatibus
+          obcaecati.
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 <script>
 export default {
--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -4,12 +4,29 @@
       <div class="col-md-7">
         <img src="https://picsum.photos/id/289/600/400" alt="khashayar" />
       </div>
-      <div class="col-md-5">
+      <div class="col-md-5 my-5">
         <h1 class="font-weight-light">Welcome...</h1>
-        <p class="mt-4">My name is Khashayar Shomali, I'm Junior Frontend Developer and this is
-          my first landing page I made by vue3 and bootstrap5.
+        <p class="mt-4">
+          My name is <mark> Khashayar Shomali</mark>, I'm Junior Frontend
+          Developer and this is my first landing page I made by vue3 and
+          bootstrap5.
         </p>
-        <button type="button" class="btn btn-primary">Check my portfolio</button>
+        <button
+          type="button"
+          class="btn btn-primary btn-lg"
+          @click="
+            openInNewTab('https://uixwithme.com/khashayar-shomali-portfolio/')
+          "
+        >
+          Check my portfolio
+        </button>
+        <button
+          type="button"
+          class="btn btn-outline-primary btn-lg my-4"
+          @click="openInNewTab('https://github.com/shomalikhashayar')"
+        >
+          Visit my GitHub
+        </button>
       </div>
     </div>
   </div>
@@ -18,7 +35,19 @@
 <script>
 export default {
   name: "Jumbotron",
+  methods: {
+    openInNewTab(url) {
+      window.open(url, "_blank", "noreferrer");
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+P {
+  font-size: 22px;
+}
+button {
+  margin: 5px;
+}
+</style>
