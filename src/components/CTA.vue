87cfd34125cc5496959418bd2ffd8f193071160a
Khashayar Shomali <khashi792017@gmail.com>
2023-01-10 18:58:39 +0330

    Add card style to my paragraph

--- src/components/CTA.vue
+++ src/components/CTA.vue
@@ -2,22 +2,24 @@
   <div class="container">
     <div class="row">
       <div class="col">
-        <div class="bg-white my-5 py-4">
-          Lorem ipsum dolor sit amet. Et amet saepe ab quae sunt et vero ipsa in
-          labore consequatur! Quo ratione quasi hic maxime laudantium et
-          reiciendis repudiandae rem exercitationem architecto. Qui distinctio
-          incidunt ut tempore quos eum fugiat ipsam aut molestiae debitis eum
-          quia magni aut aspernatur iste ex tenetur consequatur. Non beatae
-          doloribus est enim voluptas et deserunt dolor et perferendis
-          exercitationem. Qui voluptatem ullam non quos voluptatem est
-          doloremque soluta. Quo recusandae delectus aut sapiente voluptas cum
-          dolor excepturi a repellendus itaque. Eos perferendis quis et
-          aspernatur laudantium et commodi sunt ut error sapiente aut illum
-          fuga. Sit voluptatem perferendis et tempore sequi sed nobis galisum
-          eum iusto doloremque. Aut dolores velit ex fugiat aliquam et unde
-          distinctio aut nihil internos ab dignissimos exercitationem qui ullam
-          repudiandae. Et minus reprehenderit aut dolor culpa ut voluptatibus
-          obcaecati.
+        <div class="bg-white my-5 py-4 card">
+          <div class="card-body">
+            Lorem ipsum dolor sit amet. Et amet saepe ab quae sunt et vero ipsa
+            in labore consequatur! Quo ratione quasi hic maxime laudantium et
+            reiciendis repudiandae rem exercitationem architecto. Qui distinctio
+            incidunt ut tempore quos eum fugiat ipsam aut molestiae debitis eum
+            quia magni aut aspernatur iste ex tenetur consequatur. Non beatae
+            doloribus est enim voluptas et deserunt dolor et perferendis
+            exercitationem. Qui voluptatem ullam non quos voluptatem est
+            doloremque soluta. Quo recusandae delectus aut sapiente voluptas cum
+            dolor excepturi a repellendus itaque. Eos perferendis quis et
+            aspernatur laudantium et commodi sunt ut error sapiente aut illum
+            fuga. Sit voluptatem perferendis et tempore sequi sed nobis galisum
+            eum iusto doloremque. Aut dolores velit ex fugiat aliquam et unde
+            distinctio aut nihil internos ab dignissimos exercitationem qui
+            ullam repudiandae. Et minus reprehenderit aut dolor culpa ut
+            voluptatibus obcaecati.
+          </div>
         </div>
       </div>
     </div>
@@ -28,4 +30,10 @@ export default {
   name: "CTA",
 };
 </script>
-<style></style>
+<style>
+.card-body {
+
+    padding-left: 45px;
+    padding-right: 45px;
+}
+</style>
