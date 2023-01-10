1e1bcd986d87a5592bc453182cfb8b026323aa82
Khashayar Shomali <khashi792017@gmail.com>
2023-01-10 19:33:15 +0330

    last commit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,19 @@
-<template></template>
+<template>
+  <footer class="footer py-3 bg-white mt-5">
+    <div class="container">
+      <span>©️2023 Made with ❤️ by Khashayar Shomali.</span>
+    </div>
+  </footer>
+</template>
 
 <script>
 export default {
-    name:"Footer"
+  name: "Footer",
 };
 </script>
-<style></style>
+<style>
+footer {
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.3);
+  text-align: center;
+}
+</style>
