59957f7a37f77a9f46c58b801b586584dfa9e3bd
Khashayar Shomali <khashi792017@gmail.com>
2023-01-10 17:55:55 +0330

    create Jumbotron component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <Navbar msg="Welcome to Your Vue.js App" />
+  <Navbar />
+  <Jumbotron />
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
+import Jumbotron from "./components/Jumbotron.vue";
 
 export default {
   name: "App",
   components: {
     Navbar,
+    Jumbotron,
   },
 };
 </script>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,22 @@
+<template>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-7"><img src="Khashayar Shomali.png" alt=""></div>
+    <div class="col-md-5">
+      <h1 class="font-weight-light">Tagline</h1>
+      <p class="mt-4">Howdy, my name is Khashayar Shomali</p>
+      <button type="button" class="btn btn-primary">Call to Action</button>
+    </div>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: "Jumbotron",
+};
+</script>
+
+<style scoped></style>
