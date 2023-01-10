1fd46d90000b0bba12429b817133fd89531ba31b
Khashayar Shomali <khashi792017@gmail.com>
2023-01-10 19:11:43 +0330

    make them 3

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -2,15 +2,41 @@
   <div class="container">
     <div class="row">
       <div class="col-md-4">
-        <div class="card" style="width: 18rem">
+        <div class="card">
           <img src="https://picsum.photos/id/289/600/200" class="card-img-top" alt="..." />
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">vue-bootstrap-table-pagination</h5>
             <p class="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn btn-primary">Source Code</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img src="https://picsum.photos/id/289/600/200" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">vue-bootstrap-table-pagination</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Source Code</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img src="https://picsum.photos/id/289/600/200" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">vue-bootstrap-table-pagination</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Source Code</a>
           </div>
         </div>
       </div>
